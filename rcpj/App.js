eac13839b11beb58f2854f3e9506c1b9c6681b2b
lil3le <aliobeidat266@gmail.com>
2024-07-27 15:59:22 +0300

    Update App.js

--- rcpj/App.js
+++ rcpj/App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Task16 from './Tasks/task16';
+import Task17 from './Tasks/task17';
 export default function App() {
   return (
     <View style={styles.container}>
       <Task16 />
+      <Task17 />
     </View>
   );
 }
