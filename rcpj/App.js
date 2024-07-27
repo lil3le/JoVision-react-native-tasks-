4f58230d1d04219d2f4c2ff0e853e3102bb5de34
lil3le <aliobeidat266@gmail.com>
2024-07-27 16:47:38 +0300

    Update App.js

--- rcpj/App.js
+++ rcpj/App.js
@@ -4,12 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import Task16 from './Tasks/task16';
 import Task17 from './Tasks/task17';
 import Task18 from './Tasks/task18';
+import Task19 from './Tasks/task19';
+import Task20 from './Tasks/task20';
 export default function App() {
   return (
     <View style={styles.container}>
      {/* <Task16 />
       <Task17 />
-      <Task18 />*/}
+      <Task18 />
+      <Task19 />*/}
+      <Task20 />
     </View>
   );
 }
