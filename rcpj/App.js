e85495effafeaec91232234c3f960af2b6a7f39d
lil3le <aliobeidat266@gmail.com>
2024-08-05 19:40:50 +0300

    Update App.js

--- rcpj/App.js
+++ rcpj/App.js
@@ -8,6 +8,7 @@ import Task19 from './Tasks/task19';
 import Task20 from './Tasks/task20';
 import Task21 from './Tasks/task21';
 import Task22 from './Tasks/task22';
+import Task23 from './Tasks/task23';
 export default function App() {
   return (
     <View style={styles.container}>
@@ -18,6 +19,7 @@ export default function App() {
       <Task20 />
       <Task21 />*/}
       <Task22 />
+     
       
     </View>
   );
