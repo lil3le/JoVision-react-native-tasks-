d58dbbb461c8813d8e0e6a47310dd24f66f7e470
lil3le <aliobeidat266@gmail.com>
2024-08-19 15:37:52 +0300

    Create task32.js

---
+++ rcpj/Tasks/task32.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { View, Image, StyleSheet, Pressable } from 'react-native';
+import Video from 'react-native-video';
+
+const Task32 = () => {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+  const handlePress = () => {
+    setIsVideoPlaying(true);
+  };
+
+  return (
+    
+    <View style={styles.container}>
+      {!isVideoPlaying ? (
+        <Pressable onPress={handlePress}>
+          <Image source={require('../Resources/nggyu.jpg')} style={styles.image} />
+        </Pressable>
+      ) : (
+        <Video
+          source={require('../Resources/nggyu.mp4')}
+          style={styles.video}
+          controls
+          resizeMode="contain"
+        />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: 450,
+    height: 420,
+    resizeMode: 'contain',
+  },
+  video: {
+    width: 410,
+    height: 430,
+  },
+});
+
+export default Task32;
