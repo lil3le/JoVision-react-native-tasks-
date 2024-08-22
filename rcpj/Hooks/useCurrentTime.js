7127519e495d06db917e4ac4e77f34fa3d387629
lil3le <aliobeidat266@gmail.com>
2024-08-22 15:47:44 +0300

    Create useCurrentTime.js

---
+++ rcpj/Hooks/useCurrentTime.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const useCurrentTime = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId); 
+    };
+  }, []);
+
+  return currentTime;
+};
+
+export default useCurrentTime;
